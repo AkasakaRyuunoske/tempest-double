2815e09d279ac3ce2a235e0e793b33e7d72b31b2
alemassim0 <alessandromassimo04@gmail.com>
2024-11-04 20:07:06 +0100

    creazione home page, inserimento immagini e animazioni visive

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,1 +1,20 @@
-console.log("JS loaded correctly")
+function toggleMenu() {
+    const menu = document.getElementById("dropdown-menu");
+    const body = document.body;
+    menu.classList.toggle("show");
+    body.classList.toggle("menu-open");
+}
+
+// Funzione per attivare l'animazione quando l'elemento è visibile
+function handleScrollAnimation() {
+    const elements = document.querySelectorAll('.animate-slide-in-left');
+    elements.forEach(element => {
+        const position = element.getBoundingClientRect();
+        if (position.top < window.innerHeight && position.bottom >= 0) {
+            element.classList.add('show');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScrollAnimation);
+window.addEventListener('load', handleScrollAnimation); // Esegui all'avvio della pagina
