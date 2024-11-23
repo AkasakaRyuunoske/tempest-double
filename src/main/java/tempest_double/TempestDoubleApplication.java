25cbef8cc5453326b33ad61f950a4c07ec9d8138
Akasaka Ryuunosuke <warufuzake1@gmail.com>
2024-11-23 15:38:52 +0100

    R.I.P Terry A. Davis.

--- src/main/java/tempest_double/TempestDoubleApplication.java
+++ src/main/java/tempest_double/TempestDoubleApplication.java
@@ -10,8 +10,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TempestDoubleApplication {
 	/**
-	 * Материя отделилась от своей задуманной формы, надо соединить их вновь.
-	 *  Быть всем, не состояться ни в чем.
+	“What’s reality?
+
+	 I don’t know.
+	 When my bird was looking at my computer monitor I thought, ‘That bird has no idea what he’s looking at.’
+	 And yet what does the bird do? Does he panic? No, he can’t really panic, he just does the best he can.
+	 Is he able to live in a world where he’s so ignorant? Well, he doesn’t really have a choice.
+	 The bird is okay even though he doesn’t understand the world.
+	 You’re that bird looking at the monitor, and you’re thinking to yourself, ‘I can figure this out.’
+	 Maybe you have some bird ideas.
+	 Maybe that’s the best you can do.”
+
+	 ― Terry A. Davis
 	 */
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure().load();
