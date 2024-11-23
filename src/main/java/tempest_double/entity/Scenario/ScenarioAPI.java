4a4bb16bd4a9de14d5367a45c0b95e343ac936e3
Akasaka Ryuunosuke <warufuzake1@gmail.com>
2024-11-23 10:37:57 +0100

    aggiunta API: DELETE, POST, PUT

--- src/main/java/tempest_double/entity/Scenario/ScenarioAPI.java
+++ src/main/java/tempest_double/entity/Scenario/ScenarioAPI.java
@@ -1,12 +1,12 @@
 package tempest_double.entity.Scenario;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tempest_double.entity.Asset.Asset;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/api/v1/")
 @RestController
@@ -22,5 +22,20 @@ public class ScenarioAPI {
     @GetMapping("/scenario/{id}")
     Scenario getScenario(@PathVariable int id){
         return scenarioService.getScenario(id);
+    }
+
+    @PostMapping("/scenario")
+    ResponseEntity<Map<String, String>> postScenario(@RequestBody Scenario scenario){
+        return scenarioService.postScenario(scenario);
+    }
+
+    @DeleteMapping("/scenario/{id}")
+    ResponseEntity<Map<String, String>> deleteScenario(@PathVariable int id){
+        return scenarioService.deleteScenario(id);
+    }
+
+    @PutMapping("/scenario/{id}")
+    ResponseEntity<Map<String, String>> postScenario(@RequestBody Scenario scenario, @PathVariable int id){
+        return scenarioService.updateScenario(id, scenario);
     }
 }
--- src/main/java/tempest_double/entity/Scenario/ScenarioService.java
+++ src/main/java/tempest_double/entity/Scenario/ScenarioService.java
@@ -1,10 +1,20 @@
 package tempest_double.entity.Scenario;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import java.util.List;
+import java.util.Map;
 
 public interface ScenarioService {
 
     List<Scenario> getScenarios();
 
     Scenario getScenario(int id);
+
+    ResponseEntity<Map<String, String>> postScenario(Scenario scenario);
+
+    ResponseEntity<Map<String, String>> deleteScenario(int id);
+
+    ResponseEntity<Map<String, String>> updateScenario(int id, @RequestBody Scenario scenario);
 }
