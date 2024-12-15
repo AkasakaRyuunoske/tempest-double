c6bbf8223916a1d712784fa5afb6c048de0fcd5a
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2024-12-15 10:56:37 +0100

    aggiunta entita simulation

---
+++ src/main/java/tempest_double/entity/Simulation/SimulationServiceImplementation.java
@@ -0,0 +1,7 @@
+package tempest_double.entity.Simulation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimulationServiceImplementation implements SimulationService{
+}
