fd2cdfbd5676af84d7e7b393cd76decbcea85b16
AkasakaRyuunoske <akasakaryuunoske1@gmail.com>
2024-12-15 15:18:23 +0100

    aggiunto test per metodo post in situazione perfetta

--- src/test/java/tempest_double/AssetsAPITests.java
+++ src/test/java/tempest_double/AssetsAPITests.java
@@ -6,25 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tempest_double.backEndAPI.service.AssetAPI;
+import tempest_double.entity.Asset.Asset;
+import tempest_double.entity.Asset.AssetRepository;
 import tempest_double.entity.Asset.AssetServiceImplementation;
 import tempest_double.frontEndAPI.AssetsController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AssetsController.class)
+@WebMvcTest(AssetAPI.class)
 public class AssetsAPITests {
     @MockBean
     AssetServiceImplementation assetServiceImplementation;
 
+    @MockBean
+    AssetRepository assetRepository;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void testPostAssetExceptToSuccess() throws Exception {
+        Map<String, Object> assetConfiguration = new HashMap<>();
+        assetConfiguration.put("nominal_power", 4500);
 
-        Mockito.when(assetServiceImplementation.printAsset()).thenReturn("implemented for test"); // Mock service behavior
+        Asset asset = new Asset(1, "Solar Panel", assetConfiguration, "Panello solare", "producer");
 
-        mockMvc.perform(get("/assets")).andExpect(status().isOk()); // Checks for HTTP 200 status
+        Mockito.when(assetServiceImplementation.getAsset(1)).thenReturn(asset); // Mock service behavior
+
+        mockMvc.perform(get("/api/v1/asset/1"))
+                .andExpect(status().isOk()) // Check for HTTP 200
+                .andExpect(MockMvcResultMatchers.jsonPath("$.type").value("Solar Panel"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.configuration.nominal_power").value(4500))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Panello solare"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.role").value("producer"));
+
+        Mockito.verify(assetServiceImplementation).getAsset(1);
     }
 }
