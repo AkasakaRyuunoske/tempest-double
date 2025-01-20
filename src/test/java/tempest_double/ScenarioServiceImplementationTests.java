b3a9004e281efda2f8509990fc968a083b414482
alemassim0 <alessandromassimo04@gmail.com>
2025-01-20 15:59:29 +0100

    modifiche test scenario

---
+++ src/test/java/tempest_double/ScenarioServiceImplementationTests.java
@@ -0,0 +1,42 @@
+package tempest_double;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import tempest_double.entity.Scenario.Scenario;
+import tempest_double.entity.Scenario.ScenarioRepository;
+import tempest_double.entity.Scenario.ScenarioServiceImplementation;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ScenarioServiceImplementationTests {
+
+    @Mock
+    private ScenarioRepository scenarioRepository;
+
+    @InjectMocks
+    private ScenarioServiceImplementation scenarioService;
+
+    public ScenarioServiceImplementationTests() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetScenarios() {
+        when(scenarioRepository.findAll()).thenReturn(Collections.emptyList());
+
+        List<Scenario> scenarios = scenarioService.getScenarios();
+
+        assertNotNull(scenarios);
+        assertTrue(scenarios.isEmpty());
+        verify(scenarioRepository, times(1)).findAll();
+    }
+
